e517c1d5fc78b2d44ade38f4a04d81424d718b8c
powerbarrusa <barr@covrigaru.com>
2019-05-30 15:44:56 -0600

    styling is centered for todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'todo-app'
+  todoArray = [] 
+  addTodo = (value) => this.todoArray.push(value)
 }
+
